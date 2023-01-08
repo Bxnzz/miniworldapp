1172b6ac52ea6e2935cb618651e7bdba2b77e193
J4m33131 <jameloveinw@gmail.com>
2023-01-08 23:53:59 +0700

    res

--- lib/page/register.dart
+++ lib/page/register.dart
@@ -13,7 +13,7 @@ class _RegisterPageState extends State<RegisterPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Register'),
+        title: Text('Registers'),
       ),
     );
   }
