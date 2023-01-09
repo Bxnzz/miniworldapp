97a822c22547642abb4136326b97813daab3e31f
J4m33131 <jameloveinw@gmail.com>
2023-01-09 19:26:57 +0700

    update

--- lib/page/uploadImage,video.dart
+++ lib/page/uploadImage,video.dart
@@ -1,5 +1,9 @@
+import 'dart:io';
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:file_picker/file_picker.dart';
 
 class UploadPage extends StatefulWidget {
   const UploadPage({super.key});
@@ -9,12 +13,99 @@ class UploadPage extends StatefulWidget {
 }
 
 class _UploadPageState extends State<UploadPage> {
+  PlatformFile? pickedFile;
+  UploadTask? uploadTask;
+
+  get urlDownload => null;
+
+  Future uploadFile() async {
+    final path = 'files/${pickedFile!.name}';
+    final file = File(pickedFile!.path!);
+
+    final ref = FirebaseStorage.instance.ref().child(path);
+
+    setState(() {
+      uploadTask = ref.putFile(file);
+    });
+    final snapshot = await uploadTask!.whenComplete(() {});
+
+    final urlDowload = await snapshot.ref.getDownloadURL();
+    print('Dowload Link:$urlDownload');
+
+    setState(() {
+      uploadTask = null;
+    });
+  }
+
+  Future selectFile() async {
+    final result = await FilePicker.platform.pickFiles();
+    if (result == null) return;
+
+    setState(() {
+      pickedFile = result.files.first;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Uploadd'),
       ),
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          if (pickedFile != null)
+            Expanded(
+              child: Container(
+                color: Colors.blue[100],
+                child: Image.file(File(pickedFile!.path!),
+                    width: double.infinity, fit: BoxFit.cover),
+              ),
+            ),
+          ElevatedButton(
+            child: const Text('Select Image'),
+            onPressed: selectFile,
+          ),
+          const SizedBox(height: 32),
+          buildProgress(),
+          ElevatedButton(
+            child: const Text('Upload Image'),
+            onPressed: uploadFile,
+          ),
+        ],
+      )),
     );
   }
+
+  Widget buildProgress() => StreamBuilder<TaskSnapshot>(
+      stream: uploadTask?.snapshotEvents,
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          final data = snapshot.data!;
+          double progress = data.bytesTransferred / data.totalBytes;
+          return SizedBox(
+            height: 50,
+            child: Stack(
+              fit: StackFit.expand,
+              children: [
+                LinearProgressIndicator(
+                  value: progress,
+                  backgroundColor: Colors.grey,
+                  color: Colors.green,
+                ),
+                Center(
+                  child: Text(
+                    '${(100 * progress).roundToDouble()}%',
+                    style: const TextStyle(color: Colors.white),
+                  ),
+                )
+              ],
+            ),
+          );
+        } else {
+          return const SizedBox(height: 50);
+        }
+      });
 }
