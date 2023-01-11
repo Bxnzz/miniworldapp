bc307dff0233fccf645cd764258f5c7156c148e2
J4m33131 <jameloveinw@gmail.com>
2023-01-11 17:48:30 +0700

    updateNotifyAndUpload

--- lib/page/notification.dart
+++ lib/page/notification.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:onesignal_flutter/onesignal_flutter.dart';
 
 class NontificationPage extends StatefulWidget {
   const NontificationPage({super.key});
@@ -8,12 +9,23 @@ class NontificationPage extends StatefulWidget {
   State<NontificationPage> createState() => _NontificationPageState();
 }
 
-class _NontificationPageState extends  State<NontificationPage>{
+class _NontificationPageState extends State<NontificationPage> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    OneSignal.shared.setLogLevel(OSLogLevel.debug, OSLogLevel.none);
+
+    OneSignal.shared.setAppId("9670ea63-3a61-488a-afcf-8e1be833f631");
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title:Text('Nontification') ,),
+        title: Text('Nontification'),
+      ),
+      body: const Center(child: Text("OneSignal Pushnotification")),
     );
   }
 }
