4cb984af1327e514f5af1e85a1511bf047adf778
Bxnzz <114279249+Bxnzz@users.noreply.github.com>
2023-01-26 18:40:06 +0700

    fix Icon not show

--- lib/main.dart
+++ lib/main.dart
@@ -33,7 +33,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Mini world race',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
