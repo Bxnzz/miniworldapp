d2cdaccfa5b9b7773f8a31b9c32526d950ff379e
Bxnzz <114279249+Bxnzz@users.noreply.github.com>
2023-04-03 23:46:18 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -46,7 +46,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         useMaterial3: true,
       ),
-      home: const HomeAll(),
+      home: const Login(),
     );
   }
 }
