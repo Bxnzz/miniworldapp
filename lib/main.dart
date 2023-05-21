4fa111c5bb00f26944f6c6da2d94b53c53a9feca
Bxnzz <114279249+Bxnzz@users.noreply.github.com>
2023-05-22 02:41:45 +0700

    Squashed commit of the following:
    commit 4f4685d7cacfbac61ae1b292c3cec78f17032102
    Merge: 32a38fb 0863ce9
    Author: Bxnzz <114279249+Bxnzz@users.noreply.github.com>
    Date:   Mon May 22 02:31:39 2023 +0700
    
        Merge branch 'feature/210523-loginLayout' into develop
    
    commit 0863ce9c3879524c7e59a79e4d7e3f72650a64f6
    Author: Bxnzz <114279249+Bxnzz@users.noreply.github.com>
    Date:   Sun May 21 23:09:23 2023 +0700
    
        updateUi
    
    commit 32a38fbd36cb1b5add6b1cde9368fc8b2ee15475
    Author: Bxnzz <114279249+Bxnzz@users.noreply.github.com>
    Date:   Sun May 21 19:53:06 2023 +0700
    
        updateTheme

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:intl/intl.dart';
-import 'package:miniworldapp/model/login.dart';
+
 import 'package:miniworldapp/page/General/home_all.dart';
 import 'package:miniworldapp/page/General/register.dart';
 import 'package:miniworldapp/page/General/login.dart';
@@ -9,6 +9,7 @@ import 'package:miniworldapp/page/Player/createTeam.dart';
 import 'package:miniworldapp/page/home.dart';
 import 'package:miniworldapp/page/loginpage.dart';
 import 'package:miniworldapp/service/provider/appdata.dart';
+import 'package:miniworldapp/theme/default.dart';
 import 'package:provider/provider.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
@@ -17,8 +18,9 @@ import 'package:flutter_facebook_keyhash/flutter_facebook_keyhash.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-   Intl.defaultLocale = "th";
-  initializeDateFormatting(); 
+  Intl.defaultLocale = "th";
+
+  initializeDateFormatting();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
@@ -32,21 +34,32 @@ Future<void> main() async {
     ChangeNotifierProvider(
       create: (context) => AppData(),
     )
-  ], child: const MyApp()));
+  ], child:  MyApp()));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final DefaultTheme defaultTheme = DefaultTheme();
+   MyApp({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Mini world race',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        useMaterial3: true,
-      ),
+      
+      themeMode: ThemeMode.system,
+       theme: defaultTheme.flexTheme.theme.copyWith(
+                      scaffoldBackgroundColor: Colors.white,
+                      inputDecorationTheme: defaultTheme.flexTheme.theme.inputDecorationTheme.copyWith(
+                        contentPadding: const EdgeInsets.fromLTRB(6, 10, 6, 3),
+                        isDense: true,
+                      )),
+                  darkTheme: defaultTheme.flexTheme.darkTheme.copyWith(
+                      inputDecorationTheme: defaultTheme.flexTheme.darkTheme.inputDecorationTheme.copyWith(
+                    contentPadding: const EdgeInsets.fromLTRB(6, 10, 6, 3),
+                    isDense: true,
+                  )),
+    
       home: const Login(),
     );
   }
