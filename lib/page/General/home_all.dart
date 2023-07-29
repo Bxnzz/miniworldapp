b685225857ed1b2e9d940cabdd25bcb38b6f2731
J4m33131 <jameloveinw@gmail.com>
2023-07-29 21:48:26 +0700

    update

--- lib/page/General/home_all.dart
+++ lib/page/General/home_all.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'dart:developer';
 
+import 'package:anim_search_bar/anim_search_bar.dart';
 import 'package:animations/animations.dart';
 import 'package:buddhist_datetime_dateformat/buddhist_datetime_dateformat.dart';
 import 'package:dio/dio.dart';
@@ -9,6 +10,7 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:get/get.dart';
 import 'package:intl/intl.dart';
 import 'package:miniworldapp/page/General/detil_race.dart';
+import 'package:miniworldapp/page/General/home_join_detail.dart';
 
 import 'package:miniworldapp/page/General/login.dart';
 import 'package:miniworldapp/page/Host/race_create.dart';
@@ -41,10 +43,14 @@ class _HomeAllState extends State<HomeAll> {
   final transitionType = ContainerTransitionType.fade;
   String Username = '';
   String _text = '';
+  TextEditingController textController = TextEditingController();
+  List<Race> races = [];
+  late RaceService raceService;
 
   @override
   void initState() {
     super.initState();
+    raceService = RaceService(Dio(), baseUrl: context.read<AppData>().baseurl);
 
     Username = context.read<AppData>().Username;
     log(Username);
@@ -81,7 +87,7 @@ class _HomeAllState extends State<HomeAll> {
               label: 'เข้าชมการแข่งขัน',
               onPressed: () {
                 setState(() {
-                  _text = '"เข้าชมการแข่งขัน"';
+                  _text = '"เข้าชมการแข่งขัน" ';
                 });
               },
             ),
@@ -108,7 +114,6 @@ class _HomeAllState extends State<HomeAll> {
         ),
         appBar: AppBar(
           automaticallyImplyLeading: false,
-
           elevation: 0,
           flexibleSpace: Container(
             decoration: const BoxDecoration(
@@ -120,17 +125,29 @@ class _HomeAllState extends State<HomeAll> {
                     colors: [
                       Colors.purpleAccent,
                       Color.fromARGB(255, 144, 64, 255),
-                     
                     ])),
           ),
           // backgroundColor: Theme.of(context).colorScheme.primary,
-          title: Builder(
-            builder: (context) => IconButton(
-              icon: FaIcon(FontAwesomeIcons.alignLeft,
-                  color: Theme.of(context).colorScheme.onPrimary, size: 18),
-              onPressed: () => Scaffold.of(context).openDrawer(),
-            ),
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Builder(
+                builder: (context) => IconButton(
+                  icon: FaIcon(FontAwesomeIcons.alignLeft,
+                      color: Theme.of(context).colorScheme.onPrimary, size: 18),
+                  onPressed: () => Scaffold.of(context).openDrawer(),
+                ),
+              ),
+            ],
           ),
+          actions: [
+            IconButton(
+              icon: FaIcon(FontAwesomeIcons.search),
+              onPressed: () {
+                showSearch(context: context, delegate: mySearchDelegate());
+              },
+            )
+          ],
           centerTitle: false,
           titleSpacing: 0,
           //  actions: <Widget>[Text(Username)],
@@ -168,7 +185,7 @@ class _HomeAllState extends State<HomeAll> {
           ],
         ),
         drawer: SizedBox(
-          width: 200,
+          width: Get.width / 1.3,
           child: Drawer(
             backgroundColor: Get.theme.colorScheme.onPrimary,
             child: ListView(
@@ -232,6 +249,126 @@ class _HomeAllState extends State<HomeAll> {
   }
 }
 
+class mySearchDelegate extends SearchDelegate {
+  int raceID = 0;
+  @override
+  Widget? buildLeading(BuildContext context) => IconButton(
+      icon: const Icon(Icons.arrow_back),
+      onPressed: () {
+        close(context, null);
+      });
+
+  @override
+  List<Widget>? buildActions(BuildContext context) => [
+        IconButton(
+          icon: const Icon(Icons.clear),
+          onPressed: () {
+            if (query.isEmpty) {
+              close(context, null);
+            } else {
+              query = '';
+            }
+          },
+        )
+      ];
+
+  @override
+  Widget buildResults(BuildContext context) {
+    late Future<void> loadDataMethod;
+    List<Race> races = [];
+    List<Race> match = [];
+
+    late RaceService raceService;
+    raceService = RaceService(Dio(), baseUrl: context.read<AppData>().baseurl);
+    Future<void> loadData() async {
+      startLoading(context);
+      try {
+        var a = await raceService.races();
+        races = a.data;
+
+        for (var rac in races) {
+          if (rac.raceName.toLowerCase().contains(query.toLowerCase())) {
+            match.add(rac);
+          }
+        }
+      } catch (err) {
+        log('Error:$err');
+      } finally {
+        stopLoading();
+      }
+    }
+
+    loadDataMethod = loadData();
+    return FutureBuilder(
+      future: loadDataMethod,
+      builder: (BuildContext context, AsyncSnapshot snapshot) {
+        return ListView.builder(
+          itemCount: match.length,
+          itemBuilder: (context, index) {
+            loadDataMethod = loadData();
+            var result = match[index];
+            return ListTile(
+              onTap: () {
+                context.read<AppData>().idrace = result.raceId;
+                log("race id =${result.raceId}");
+                Get.to(() => DetailRace());
+                loadDataMethod = loadData();
+              },
+              title: Text(result.raceName),
+            );
+          },
+        );
+      },
+    );
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    late Future<void> loadDataMethod;
+    List<Race> races = [];
+    List<Race> match = [];
+
+    late RaceService raceService;
+    raceService = RaceService(Dio(), baseUrl: context.read<AppData>().baseurl);
+    Future<void> loadData() async {
+      try {
+        var a = await raceService.races();
+        races = a.data;
+
+        for (var rac in races) {
+          if (rac.raceName.toLowerCase().contains(query.toLowerCase())) {
+            match.add(rac);
+          }
+        }
+      } catch (err) {
+        log('Error:$err');
+      }
+    }
+
+    loadDataMethod = loadData();
+    return FutureBuilder(
+      future: loadDataMethod,
+      builder: (BuildContext context, AsyncSnapshot snapshot) {
+        return ListView.builder(
+          itemCount: match.length,
+          itemBuilder: (context, index) {
+            var result = match[index];
+            return ListTile(
+              onTap: () {
+                context.read<AppData>().idrace = result.raceId;
+                log("race id =${result.raceId}");
+                Get.to(() => DetailRace());
+                loadDataMethod = loadData();
+              },
+              title: Text(result.raceName),
+            );
+          },
+        );
+      },
+    );
+  }
+}
+
 class RaceAll extends StatefulWidget {
   const RaceAll({super.key});
 
@@ -270,16 +407,16 @@ class _RaceAllState extends State<RaceAll> {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.onPrimary,
       body: Container(
-         decoration: const BoxDecoration(
-                gradient: LinearGradient(
-                    begin: FractionalOffset(0.0, 0.0),
-                    end: FractionalOffset(1.0, 0.0),
-                    stops: [0.0, 1.0],
-                    tileMode: TileMode.clamp,
-                    colors: [
-                      Colors.purpleAccent,
-                      Color.fromARGB(255, 144, 64, 255),
-                    ])),
+        decoration: const BoxDecoration(
+            gradient: LinearGradient(
+                begin: FractionalOffset(0.0, 0.0),
+                end: FractionalOffset(1.0, 0.0),
+                stops: [0.0, 1.0],
+                tileMode: TileMode.clamp,
+                colors: [
+                  Colors.purpleAccent,
+                  Color.fromARGB(255, 144, 64, 255),
+                ])),
         child: FutureBuilder(
             future: loadDataMethod,
             builder: (context, AsyncSnapshot snapshot) {
@@ -289,21 +426,21 @@ class _RaceAllState extends State<RaceAll> {
                   padding: EdgeInsets.only(top: 10),
                   children: races.map((element) {
                     return Padding(
-                      padding:
-                          const EdgeInsets.only(left: 2.5, right: 2.5, bottom: 5),
+                      padding: const EdgeInsets.only(
+                          left: 2.5, right: 2.5, bottom: 5),
                       child: Card(
-                         shape: RoundedRectangleBorder(
-                              side: BorderSide(
-                                width: 2,
-                                color: Colors.white,
-                              ),
-                              borderRadius:
-                                  BorderRadius.circular(20.0), //<-- SEE HERE
-                            ),
+                        shape: RoundedRectangleBorder(
+                          side: BorderSide(
+                            width: 2,
+                            color: Colors.white,
+                          ),
+                          borderRadius:
+                              BorderRadius.circular(20.0), //<-- SEE HERE
+                        ),
                         //  shadowColor: ,
                         color: Colors.white,
                         clipBehavior: Clip.hardEdge,
-      
+
                         child: InkWell(
                           borderRadius: BorderRadius.circular(12.0),
                           splashColor: Colors.blue.withAlpha(30),
@@ -323,7 +460,8 @@ class _RaceAllState extends State<RaceAll> {
                               footer: Container(
                                 color: Get.theme.colorScheme.onBackground
                                     .withOpacity(0.5),
-                                padding: const EdgeInsets.fromLTRB(10, 5, 10, 0),
+                                padding:
+                                    const EdgeInsets.fromLTRB(10, 5, 10, 0),
                                 child: Column(
                                   crossAxisAlignment: CrossAxisAlignment.start,
                                   children: [
@@ -349,11 +487,12 @@ class _RaceAllState extends State<RaceAll> {
                                     //     formatter.formatInBuddhistCalendarThai(
                                     //         element.raceTimeFn)),
                                     Text("สถานที่: " + element.raceLocation,
-                                        style: Get.textTheme.bodySmall!.copyWith(
-                                            color: Get.theme.colorScheme.onPrimary
-                                                .withOpacity(0.8))),
+                                        style: Get.textTheme.bodySmall!
+                                            .copyWith(
+                                                color: Get
+                                                    .theme.colorScheme.onPrimary
+                                                    .withOpacity(0.8))),
                                     Container(height: 5),
-                      
                                   ],
                                 ),
                               )),
@@ -362,8 +501,7 @@ class _RaceAllState extends State<RaceAll> {
                     );
                   }).toList(),
                 );
-              }
-               else {
+              } else {
                 return Container();
                 // const CircularProgressIndicator();
               }
