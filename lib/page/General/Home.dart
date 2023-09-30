505b2bd30e78637cfbd2a56fda4a9f8912f593f4
Bxnzz <114279249+Bxnzz@users.noreply.github.com>
2023-09-30 15:57:44 +0700

    update

--- lib/page/General/Home.dart
+++ lib/page/General/Home.dart
@@ -516,71 +516,73 @@ class mySearchDelegate extends SearchDelegate {
     return FutureBuilder(
       future: loadDataMethod,
       builder: (BuildContext context, AsyncSnapshot snapshot) {
-        return GridView.count(
-          crossAxisCount: 2,
+        return ListView(
           padding: EdgeInsets.only(top: 10),
           children: match.map((element) {
             return Padding(
-              padding: const EdgeInsets.only(left: 2.5, right: 2.5, bottom: 5),
-              child: Card(
-                shape: RoundedRectangleBorder(
-                  side: BorderSide(
-                    width: 2,
-                    color: Colors.white,
+              padding: const EdgeInsets.only(right: 10, left: 10),
+              child: SizedBox(
+                height: 150,
+                child: Card(
+                  shape: RoundedRectangleBorder(
+                    side: BorderSide(
+                      width: 2,
+                      color: Colors.white,
+                    ),
+                    borderRadius: BorderRadius.circular(20.0), //<-- SEE HERE
                   ),
-                  borderRadius: BorderRadius.circular(20.0), //<-- SEE HERE
-                ),
-                //  shadowColor: ,
-                color: Colors.white,
-                clipBehavior: Clip.hardEdge,
-
-                child: InkWell(
-                  borderRadius: BorderRadius.circular(12.0),
-                  splashColor: Colors.blue.withAlpha(30),
-                  onTap: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => DetailRace()));
-                    context.read<AppData>().idrace = element.raceId;
-                  },
-                  child: GridTile(
-                      // crossAxisAlignment: CrossAxisAlignment.start,
-                      child: Image.network(element.raceImage,
-                          //  width: Get.width,
-                          //  height: Get.width*0.5625/2,
-                          fit: BoxFit.cover),
-                      footer: Container(
-                        color:
-                            Get.theme.colorScheme.onBackground.withOpacity(0.5),
-                        padding: const EdgeInsets.fromLTRB(10, 5, 10, 0),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Text(element.raceName,
-                                    style: Get.textTheme.bodyMedium!.copyWith(
-                                        fontWeight: FontWeight.bold,
-                                        color:
-                                            Get.theme.colorScheme.onPrimary)),
-                                Text("# ${element.raceId}",
-                                    style: Get.textTheme.bodySmall!.copyWith(
-                                        color:
-                                            Get.theme.colorScheme.onPrimary)),
-                              ],
-                            ),
-                            Container(height: 5),
-                            // Text("ปิดรับสมัคร: " +
-                            //     formatter.formatInBuddhistCalendarThai(
-                            //         element.raceTimeFn)),
-                            Text("สถานที่: " + element.raceLocation,
-                                style: Get.textTheme.bodySmall!.copyWith(
-                                    color: Get.theme.colorScheme.onPrimary
-                                        .withOpacity(0.8))),
-                            Container(height: 5),
-                          ],
-                        ),
-                      )),
+                  //  shadowColor: ,
+                  color: Colors.white,
+                  clipBehavior: Clip.hardEdge,
+              
+                  child: InkWell(
+                    borderRadius: BorderRadius.circular(12.0),
+                    splashColor: Colors.blue.withAlpha(30),
+                    onTap: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => DetailRace()));
+                      context.read<AppData>().idrace = element.raceId;
+                    },
+                    child: GridTile(
+                        // crossAxisAlignment: CrossAxisAlignment.start,
+                        child: Image.network(element.raceImage,
+                            //  width: Get.width,
+                            //  height: Get.width*0.5625/2,
+                            fit: BoxFit.cover),
+                        footer: Container(
+                          color:
+                              Get.theme.colorScheme.onBackground.withOpacity(0.5),
+                          padding: const EdgeInsets.fromLTRB(10, 5, 10, 0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text(element.raceName,
+                                      style: Get.textTheme.bodyMedium!.copyWith(
+                                          fontWeight: FontWeight.bold,
+                                          color:
+                                              Get.theme.colorScheme.onPrimary)),
+                                  Text("# ${element.raceId}",
+                                      style: Get.textTheme.bodySmall!.copyWith(
+                                          color:
+                                              Get.theme.colorScheme.onPrimary)),
+                                ],
+                              ),
+                              Container(height: 5),
+                              // Text("ปิดรับสมัคร: " +
+                              //     formatter.formatInBuddhistCalendarThai(
+                              //         element.raceTimeFn)),
+                              Text("สถานที่: " + element.raceLocation,
+                                  style: Get.textTheme.bodySmall!.copyWith(
+                                      color: Get.theme.colorScheme.onPrimary
+                                          .withOpacity(0.8))),
+                              Container(height: 5),
+                            ],
+                          ),
+                        )),
+                  ),
                 ),
               ),
             );
