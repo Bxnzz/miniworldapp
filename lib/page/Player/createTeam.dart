140ffdcdf2ea48294a9575203f6641cefd3a762f
J4m33131 <jameloveinw@gmail.com>
2023-10-21 22:51:12 +0700

    update

--- lib/page/Player/createTeam.dart
+++ lib/page/Player/createTeam.dart
@@ -195,8 +195,12 @@ class _CeateTeamState extends State<CeateTeam> {
                                     Padding(
                                         padding: const EdgeInsets.only(
                                             top: 30, right: 30, left: 30),
-                                        child: textField(nameMember1, 'ตัวฉันเอง',
-                                            'ตัวฉันเอง', '', true)),
+                                        child: textField(
+                                            nameMember1,
+                                            'ตัวฉันเอง',
+                                            'ตัวฉันเอง',
+                                            '',
+                                            true)),
                                     Padding(
                                       padding: const EdgeInsets.only(top: 30),
                                       child: SizedBox(
@@ -236,10 +240,13 @@ class _CeateTeamState extends State<CeateTeam> {
                                                     if (raceST.isAfter(j.team.race.raceTimeSt) &&
                                                         raceST.isBefore(j.team
                                                             .race.raceTimeFn) &&
-                                                        raceFN.isAfter(j.team.race
-                                                            .raceTimeSt) &&
+                                                        raceFN.isAfter(j.team
+                                                            .race.raceTimeSt) &&
                                                         raceFN.isBefore(j.team
-                                                            .race.raceTimeFn)) {
+                                                            .race.raceTimeFn) &&
+                                                        j.team.race
+                                                                .raceStatus ==
+                                                            1) {
                                                       isJoin = true;
                                                       log("can't join chk 4 condition");
                                                       ScaffoldMessenger.of(
@@ -262,8 +269,11 @@ class _CeateTeamState extends State<CeateTeam> {
                                               'สร้างทีม',
                                               style: Get.textTheme.bodyLarge!
                                                   .copyWith(
-                                                      fontWeight: FontWeight.bold,
-                                                      color: Get.theme.colorScheme
+                                                      fontWeight:
+                                                          FontWeight.bold,
+                                                      color: Get
+                                                          .theme
+                                                          .colorScheme
                                                           .onPrimary),
                                             )),
                                       ),
@@ -280,8 +290,8 @@ class _CeateTeamState extends State<CeateTeam> {
                                   padding: const EdgeInsets.all(16),
                                   decoration: BoxDecoration(
                                     color: Color.fromARGB(255, 222, 72, 249),
-                                    border:
-                                        Border.all(color: Colors.white, width: 3),
+                                    border: Border.all(
+                                        color: Colors.white, width: 3),
                                     shape: BoxShape.rectangle,
                                     borderRadius: BorderRadius.circular(100),
                                   ),
